da926d7d0b4e0953ff9a4f360926bda00e9606c2
wafiyphd <wafiyphd@gmail.com>
2017-09-23 01:55:42 +0800

    Basis of joining sessions design (front-end only)
    Basis of joining sessions design (front-end only)

--- joinsessionslist.php
+++ joinsessionslist.php
@@ -65,7 +65,7 @@ if( isset($_POST['login']) ) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>About - ROUTE</title>
+	<title>Joining Session - ROUTE</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="icon" href="favicon.ico" type="image/x-icon"> 
