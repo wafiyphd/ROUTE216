553bd8924e53e781fd15791c603e490a073c9fea
wafiyphd <wafiyphd@gmail.com>
2017-09-28 11:05:25 +0800

    About container fix
    About container fix

--- about.php
+++ about.php
@@ -194,83 +194,83 @@ if( isset($_POST['login']) ) {
 	</div>
 
 	<div class="container-fluid info-container"> 
-		<div class = "background">	
-		<h2 class="text-center">What ROUTE Is</h2><br>
-			<div class="row">
-				<div class="col-sm-5 col-sm-offset-1">
-					<blockquote class="blockquote-reverse">
-						<h5>What We Do</h5>
-						<p>ROUTE creates a seamless experience between both trainers and members. It allows both parties to create 
-						and modify their schedules on the go. Both fitness trainers and fitness members alike can log their free 
-						<strong>time</strong> for training sessions. You can schedule both personal and group sessions with ROUTE.</p>
-					</blockquote>
-					</div>
-					<div class="col-sm-5">
-					<blockquote class="blockquote-reverse">
-						<h5>What We Want</h5>
-						<p>ROUTE aims to create a one-stop application that will synchronize your schedules, along with the schedules 
-						of other fitness trainers and members. We want you to have a more interactive relationship, allowing you to 
-						book sessions without having to meet face to face, saving time and putting the main emphasis on <strong>fitness</strong>.</p>
-					</blockquote>
-					</div>
+		<div class="container">
+			<div class = "background">	
+			<h2 class="text-center">What ROUTE Is</h2><br>
+				<div class="row">
 					<div class="col-sm-5 col-sm-offset-1">
 						<blockquote class="blockquote-reverse">
-						<h5>Who We Are</h5>
-						<p>ROUTE intends to reduce the lack of time, especially for fitness trainers and members.
-						Not everyone can be there at a gym or studio to log and book every session, and we understand that.
-						Many of us have conflicting schedules. As hectic as life can get, <strong>you</strong> can't always be 
-						there in the same gym or studio to book a session.</p>
-					</blockquote>
-					</div>
-					<div class="col-sm-5">
+							<h5>What We Do</h5>
+							<p>ROUTE creates a seamless experience between both trainers and members. It allows both parties to create 
+							and modify their schedules on the go. Both fitness trainers and fitness members alike can log their free 
+							<strong>time</strong> for training sessions. You can schedule both personal and group sessions with ROUTE.</p>
+						</blockquote>
+						</div>
+						<div class="col-sm-5">
 						<blockquote class="blockquote-reverse">
-						<h5>Why We Do It</h5>
-						<p>We know how important your fitness can be to you. We also know how important your time can be to you.
-						We don't want you to have to worry about planning your schedule around your health. We want your main
-						focus to be <strong>worry-free</strong> exercise, not having to spend time you could be putting into your fitness regime
-						into scheduling a training session.</p>
-					</blockquote>
+							<h5>What We Want</h5>
+							<p>ROUTE aims to create a one-stop application that will synchronize your schedules, along with the schedules 
+							of other fitness trainers and members. We want you to have a more interactive relationship, allowing you to 
+							book sessions without having to meet face to face, saving time and putting the main emphasis on <strong>fitness</strong>.</p>
+						</blockquote>
+						</div>
+						<div class="col-sm-5 col-sm-offset-1">
+							<blockquote class="blockquote-reverse">
+							<h5>Who We Are</h5>
+							<p>ROUTE intends to reduce the lack of time, especially for fitness trainers and members.
+							Not everyone can be there at a gym or studio to log and book every session, and we understand that.
+							Many of us have conflicting schedules. As hectic as life can get, <strong>you</strong> can't always be 
+							there in the same gym or studio to book a session.</p>
+						</blockquote>
+						</div>
+						<div class="col-sm-5">
+							<blockquote class="blockquote-reverse">
+							<h5>Why We Do It</h5>
+							<p>We know how important your fitness can be to you. We also know how important your time can be to you.
+							We don't want you to have to worry about planning your schedule around your health. We want your main
+							focus to be <strong>worry-free</strong> exercise, not having to spend time you could be putting into your fitness regime
+							into scheduling a training session.</p>
+						</blockquote>
+					</div>
 				</div>
 			</div>
-		</div>
-	</div>
 	 
-	<div class="container-fluid info-container">
-		<div class="features-wrap">
-		<h2 class = "text-center">Join Us At ROUTE</h2>
-			<div class = "row">
-				<div class = "col-sm-5 col-sm-offset-1 text-right">
-					<div class = "features">
-						<h3>As a Member</h3>
-						<blockquote class="blockquote-reverse">
-							<h5>Join training sessions</h5>
-							<h4>Register for training sessions created by your favourite trainers.</h4>
-						</blockquote>
-						<blockquote class="blockquote-reverse">
-							<h5>View your training history</h5>
-							<h4>Find your completed classes with quick and easy access.</h4>
-						</blockquote>
-						<blockquote class="blockquote-reverse">
-							<h5>Submit reviews</h5>
-							<h4>Provide trainers with feedback on how their classes went.</h4>
-						</blockquote>
+			<div class="features-wrap">
+			<h2 class = "text-center">Join Us At ROUTE</h2>
+				<div class = "row">
+					<div class = "col-sm-5 col-sm-offset-1 text-right">
+						<div class = "features">
+							<h3>As a Member</h3>
+							<blockquote class="blockquote-reverse">
+								<h5>Join training sessions</h5>
+								<h4>Register for training sessions created by your favourite trainers.</h4>
+							</blockquote>
+							<blockquote class="blockquote-reverse">
+								<h5>View your training history</h5>
+								<h4>Find your completed classes with quick and easy access.</h4>
+							</blockquote>
+							<blockquote class="blockquote-reverse">
+								<h5>Submit reviews</h5>
+								<h4>Provide trainers with feedback on how their classes went.</h4>
+							</blockquote>
+						</div>
 					</div>
-				</div>
-				<div class = "col-sm-5 text-right">
-					<div class = "features">
-						<h3>As a Trainer</h3>
-						<blockquote class="blockquote-reverse">
-							<h5>Create training sessions</h5>
-							<h4>Create personal and interactive sessions with fitness members.</h4>
-						</blockquote>
-						<blockquote class="blockquote-reverse">
-							<h5>Manage training sessions</h5>
-							<h4>Seamlessly update your training sessions with the latest information.</h4>
-						</blockquote>
-						<blockquote class="blockquote-reverse">
-							<h5>Read reviews</h5>
-							<h4>See how you well you did based on what fitness members say.</h4>
-						</blockquote>
+					<div class = "col-sm-5 text-right">
+						<div class = "features">
+							<h3>As a Trainer</h3>
+							<blockquote class="blockquote-reverse">
+								<h5>Create training sessions</h5>
+								<h4>Create personal and interactive sessions with fitness members.</h4>
+							</blockquote>
+							<blockquote class="blockquote-reverse">
+								<h5>Manage training sessions</h5>
+								<h4>Seamlessly update your training sessions with the latest information.</h4>
+							</blockquote>
+							<blockquote class="blockquote-reverse">
+								<h5>Read reviews</h5>
+								<h4>See how you well you did based on what fitness members say.</h4>
+							</blockquote>
+						</div>
 					</div>
 				</div>
 			</div>
